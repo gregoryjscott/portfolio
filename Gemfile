36cafb4f583ac4f9bc1f0c5761ffa2910ed4ab7f
Greg Scott <i@gregoryjscott.com>
2014-12-11 08:48:30 -0600

    Add handy Site monkey patcher for tests

--- Gemfile
+++ Gemfile
@@ -4,7 +4,7 @@ gem 'jekyll', '~> 2.5'
 
 group :jekyll_plugins do
   gem 'jekyll-load'
-  gem 'jekyll-embed'
   gem 'jekyll-prep'
+  gem 'jekyll-embed'
   gem 'jekyll-api'
 end
--- lib/jekyll/site_patch.rb
+++ lib/jekyll/site_patch.rb
@@ -1,10 +1,17 @@
 module Jekyll
-  # class Site
-  #
-  #   alias_method :old_process, :process
-  #
-  #   # def process()
-  #   #   old_process()
-  #   # end
-  # end
+  class Site
+
+    # alias_method :old_process, :process
+    #
+    # def process()
+    #   old_process()
+    # end
+
+    def generate
+      generators.each do |generator|
+        puts generator
+        generator.generate(self)
+      end
+    end
+  end
 end
