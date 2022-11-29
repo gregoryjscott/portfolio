8ef435ca474d6c30882f9eaa5543fab9437d0b4d
Greg Scott <me@gregoryjscott.com>
2022-11-28 18:46:33 -0600

    WIP prep _data

--- prep.ts
+++ prep.ts
@@ -1,78 +1,67 @@
+// TODO
+// - [ ] Generate the `_links` inside of ./_data/x/*.yml, where x = db, jobs, languages, os, tools using ./_data/projects and ./data/schools
+// - [ ] Generate `self` where x = db, jobs, languages, projects, schools, tools
+
 import * as fs from 'fs'
 import * as YAML from 'yaml'
 import * as path from 'path'
 
 interface Project {
   url: string
-  languages?: Language[]
+  languages?: Skill[]
 }
 
-interface Language {
+interface Skill {
   url: string
   projects?: Project[]
 }
 
-// parse _data/projects
-// Generate the `_links` inside of ./_data/x/*.yml, where x = db, jobs, languages, os, tools using ./_data/projects and ./data/schools
+const languages: Skill[] = buildSkill('languages')
+const db: Skill[] = buildSkill('db')
+updateData()
+writeSkill('db', db)
+writeSkill('languages', languages)
 
-// const dataFiles = fs.readdirSync('./_data')
-// console.log(dataFiles)
+function updateData() {
+  const projectFiles = fs.readdirSync('./_data/projects')
+  for (const projectFile of projectFiles) {
+    const projectURL = `/projects/${path.parse(projectFile).name}/`
+    const contents = fs.readFileSync(`./_data/projects/${projectFile}`, 'utf8')
+    const data = YAML.parse(contents)
 
-// const jobFiles = fs.readdirSync('./_data/jobs')
-// const jobURLs = jobFiles.map(j => `/jobs/${path.parse(j).name}`)
-// console.log(jobURLs)
-
-// const contents = fs.readFileSync('./_data/projects/epicloud.yml', 'utf8')
-// const data = YAML.parse(contents)
-// console.log(JSON.stringify(data, null, 2))
-// console.log(data._links.job[0])
-
-
-const languageFiles = fs.readdirSync('./_data/languages')
-const languageURLs = languageFiles.map(j => `/languages/${path.parse(j).name}/`)
-const languages: Language[] = []
-for (const languageURL of languageURLs) {
-  languages.push({url: languageURL, projects: []})
-}
-console.log(languages)
-
-const projectFiles = fs.readdirSync('./_data/projects')
-for (const projectFile of projectFiles) {
-  const url = `/projects/${path.parse(projectFile).name}/`
-  const contents = fs.readFileSync(`./_data/projects/${projectFile}`, 'utf8')
-  const data = YAML.parse(contents)
-//   console.log(url)
-//   console.log(data._links.languages)
-
-  for (const projectLanguage of data._links.languages) {
-    const language = languages.find(l => l.url === projectLanguage.href)
-    if (!language) throw `${projectLanguage.href} is missing`
-    language.projects.push({url})
+    updateLinkedSkill(db, projectURL, data._links.db)
+    updateLinkedSkill(languages, projectURL, data._links.languages)
   }
 }
 
-for (const language of languages) {
-  if (language.projects.length < 1) continue
-  const languageFilePath = `_data/languages/${path.parse(language.url).name}.yml`
-  console.log(languageFilePath)
-  const contents = fs.readFileSync(languageFilePath, 'utf8')
-  const data = YAML.parse(contents)
-  data._links.projects = language.projects.map(p => { return {href: p.url}})
-  fs.writeFileSync(languageFilePath, YAML.stringify(data))
+function buildSkill(urlFragment: string): Skill[] {
+  const files = fs.readdirSync(`./_data/${urlFragment}`)
+  const urls = files.map(j => `/${urlFragment}/${path.parse(j).name}/`)
+  const skills: Skill[] = []
+  for (const url of urls) {
+    skills.push({url: url, projects: []})
+  }
+  return skills
 }
 
-// console.log(JSON.stringify(languages, null, 2))
+function updateLinkedSkill(skills: Skill[], projectURL: string, links: any[]) {
+  if (links) {
+    for (const link of links) {
+      const skill = skills.find(s => s.url === link.href)
+      if (!skill) throw `${link.href} is missing`
+      skill.projects.push({url: projectURL})
+    }
+  }
+}
 
-
-
-// ******************
-// looks like jekyll-embed needs to add a _links.self to embedded so that embedded can be passed around and still contain the href
-// OR, just do it in this script..........
-// ******************
-
-
-// go through the projects
-// for each project
-//   find the list of languages
-//   for each language
-//     add the project
+function writeSkill(urlFragment: string, skills: Skill[]) {
+  for (const skill of skills) {
+    if (skill.projects.length < 1) continue
+    const filePath = `_data/${urlFragment}/${path.parse(skill.url).name}.yml`
+    console.log(filePath)
+    const contents = fs.readFileSync(filePath, 'utf8')
+    const data = YAML.parse(contents)
+    data._links.projects = skill.projects.map(p => { return {href: p.url}})
+    fs.writeFileSync(filePath, YAML.stringify(data))
+  }
+}
