f93890f19048f569f4cb43c0462a8ba63d3f9f81
Greg Scott <me@gregoryjscott.com>
2022-12-01 14:16:53 -0600

    Summarize project linked data in jobs

--- prep.ts
+++ prep.ts
@@ -4,6 +4,20 @@ import * as path from 'path'
 
 interface Project {
   url: string
+  jobs?: Job[]
+  db?: Skill[]
+  languages?: Skill[]
+  os?: Skill[]
+  tools?: Skill[]
+}
+
+interface Job {
+  url: string
+  projects?: Project[]
+  db?: Skill[]
+  languages?: Skill[]
+  os?: Skill[]
+  tools?: Skill[]
 }
 
 interface School {
@@ -16,8 +30,10 @@ interface Skill {
   schools?: School[]
 }
 
+const projects: Project[] = []
+const jobs: Job[] = buildJob('jobs')
+
 const db: Skill[] = buildSkill('db')
-const jobs: Skill[] = buildSkill('jobs')
 const languages: Skill[] = buildSkill('languages')
 const os: Skill[] = buildSkill('os')
 const tools: Skill[] = buildSkill('tools')
@@ -25,55 +41,107 @@ updateData()
 
 function updateData() {
   const projectFiles = fs.readdirSync('./_data/projects')
-  for (const projectFile of projectFiles) {
-    const projectURL = `/projects/${path.parse(projectFile).name}/`
-    const projectFilePath = `./_data/projects/${projectFile}`
-    const contents = fs.readFileSync(projectFilePath, 'utf8')
+  for (const file of projectFiles) {
+    const project = {
+      url: `/projects/${path.parse(file).name}/`,
+      jobs: [],
+      db: [],
+      languages: [],
+      os: [],
+      tools: []
+    }
+    projects.push(project)
+    const filePath = `_data/projects/${file}`
+    const contents = fs.readFileSync(filePath, 'utf8')
     const data = YAML.parse(contents)
 
     if (!data._links) data._links = {}
-    data._links.self = {href: projectURL}
+    data._links.self = {href: project.url}
     data._links.db = mapLinks(data._links.db)
     data._links.jobs = mapLinks(data._links.jobs)
     data._links.languages = mapLinks(data._links.languages)
     data._links.os = mapLinks(data._links.os)
     data._links.tools = mapLinks(data._links.tools)
-    fs.writeFileSync(projectFilePath, YAML.stringify(data))
 
-    updateProjectLinkedSkill(db, {url: projectURL}, data._links.db)
-    updateProjectLinkedSkill(jobs, {url: projectURL}, data._links.jobs)
-    updateProjectLinkedSkill(languages, {url: projectURL}, data._links.languages)
-    updateProjectLinkedSkill(os, {url: projectURL}, data._links.os)
-    updateProjectLinkedSkill(tools, {url: projectURL}, data._links.tools)
+    if (data._links.jobs) project.jobs = data._links.jobs.map(l => { return {url: l.href}})
+    if (data._links.db) project.db = data._links.db.map(l => { return {url: l.href}})
+    if (data._links.languages) project.languages = data._links.languages.map(l => { return {url: l.href}})
+    if (data._links.os) project.os = data._links.os.map(l => { return {url: l.href}})
+    if (data._links.tools) project.tools = data._links.tools.map(l => { return {url: l.href}})
+
+    fs.writeFileSync(filePath, YAML.stringify(data))
+    console.log(`Wrote ${filePath}`)
+
+    updateProjectLinkedSkill(jobs, project, data._links.jobs)
+    updateProjectLinkedSkill(db, project, data._links.db)
+    updateProjectLinkedSkill(languages, project, data._links.languages)
+    updateProjectLinkedSkill(os, project, data._links.os)
+    updateProjectLinkedSkill(tools, project, data._links.tools)
   }
 
   const schoolFiles = fs.readdirSync('./_data/schools')
-  for (const schoolFile of schoolFiles) {
-    const schoolURL = `/schools/${path.parse(schoolFile).name}/`
-    const schoolFilePath = `./_data/schools/${schoolFile}`
-    const contents = fs.readFileSync(schoolFilePath, 'utf8')
+  for (const file of schoolFiles) {
+    const school = { url: `/schools/${path.parse(file).name}/`}
+    const filePath = `_data/schools/${file}`
+    const contents = fs.readFileSync(filePath, 'utf8')
     const data = YAML.parse(contents)
 
     if (!data._links) data._links = {}
-    data._links.self = {href: schoolURL}
+    data._links.self = {href: school.url}
     data._links.db = mapLinks(data._links.db)
     data._links.languages = mapLinks(data._links.languages)
     data._links.os = mapLinks(data._links.os)
     data._links.tools = mapLinks(data._links.tools)
-    fs.writeFileSync(schoolFilePath, YAML.stringify(data))
+    fs.writeFileSync(filePath, YAML.stringify(data))
+    console.log(`Wrote ${filePath}`)
 
-    updateSchoolLinkedSkill(db, {url: schoolURL}, data._links.db)
-    updateSchoolLinkedSkill(jobs, {url: schoolURL}, data._links.jobs)
-    updateSchoolLinkedSkill(languages, {url: schoolURL}, data._links.languages)
-    updateSchoolLinkedSkill(os, {url: schoolURL}, data._links.os)
-    updateSchoolLinkedSkill(tools, {url: schoolURL}, data._links.tools)
+    updateSchoolLinkedSkill(db, school, data._links.db)
+    updateSchoolLinkedSkill(languages, school, data._links.languages)
+    updateSchoolLinkedSkill(os, school, data._links.os)
+    updateSchoolLinkedSkill(tools, school, data._links.tools)
+  }
+
+  for (const job of jobs) {
+    const dbURLs = new Set<string>()
+    const languageURLs = new Set<string>()
+    const osURLs = new Set<string>()
+    const toolURLs = new Set<string>()
+    for (const jobProject of job.projects) {
+      const project = projects.find(p => p.url === jobProject.url)
+      for (const database of project.db) {
+        dbURLs.add(database.url)
+      }
+      for (const language of project.languages) {
+        languageURLs.add(language.url)
+      }
+      for (const os of project.os) {
+        osURLs.add(os.url)
+      }
+      for (const tool of project.tools) {
+        toolURLs.add(tool.url)
+      }
+    }
+    job.db = Array.from(dbURLs).map(url => { return {url}})
+    job.languages = Array.from(languageURLs).map(url => { return {url}})
+    job.os = Array.from(osURLs).map(url => { return {url}})
+    job.tools = Array.from(toolURLs).map(url => { return {url}})
   }
 
   writeSkill('db', db)
-  writeSkill('jobs', jobs)
+  writeJob('jobs', jobs)
   writeSkill('languages', languages)
   writeSkill('os', os)
   writeSkill('tools', tools)
+}
+
+function buildJob(urlFragment: string): Job[] {
+  const files = fs.readdirSync(`./_data/${urlFragment}`)
+  const urls = files.map(j => `/${urlFragment}/${path.parse(j).name}/`)
+  const jobs: Job[] = []
+  for (const url of urls) {
+    jobs.push({url: url, projects: [], languages: []})
+  }
+  return jobs
 }
 
 function buildSkill(urlFragment: string): Skill[] {
@@ -110,6 +178,44 @@ function updateSchoolLinkedSkill(skills: Skill[], school: School, links: any[]) 
   }
 }
 
+function writeJob(urlFragment: string, jobs: Job[]) {
+  for (const job of jobs) {
+    const filePath = `_data/${urlFragment}/${path.parse(job.url).name}.yml`
+    const contents = fs.readFileSync(filePath, 'utf8')
+    const data = YAML.parse(contents)
+    if (!data._links) data._links = {}
+    const self = `/${urlFragment}/${path.parse(filePath).name}/`
+    data._links.self = {href: self}
+    if (job.projects.length > 0) {
+      data._links.projects = job.projects.map(p => { return {href: p.url}})
+    } else if (data._links.projects) {
+      delete data._links.projects
+    }
+    if (job.db.length > 0) {
+      data._links.db = job.db.map(p => { return {href: p.url}})
+    } else if (data._links.db) {
+      delete data._links.db
+    }
+    if (job.languages.length > 0) {
+      data._links.languages = job.languages.map(p => { return {href: p.url}})
+    } else if (data._links.languages) {
+      delete data._links.languages
+    }
+    if (job.os.length > 0) {
+      data._links.os = job.os.map(p => { return {href: p.url}})
+    } else if (data._links.os) {
+      delete data._links.os
+    }
+    if (job.tools.length > 0) {
+      data._links.tools = job.tools.map(p => { return {href: p.url}})
+    } else if (data._links.tools) {
+      delete data._links.tools
+    }
+    fs.writeFileSync(filePath, YAML.stringify(data))
+    console.log(`Wrote ${filePath}`)
+  }
+}
+
 function writeSkill(urlFragment: string, skills: Skill[]) {
   for (const skill of skills) {
     const filePath = `_data/${urlFragment}/${path.parse(skill.url).name}.yml`
@@ -128,7 +234,7 @@ function writeSkill(urlFragment: string, skills: Skill[]) {
     } else if (data._links.schools) {
       delete data._links.schools
     }
-    console.log(filePath)
     fs.writeFileSync(filePath, YAML.stringify(data))
+    console.log(`Wrote ${filePath}`)
   }
 }
