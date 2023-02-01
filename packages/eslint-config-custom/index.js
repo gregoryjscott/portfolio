6c34cdd5af9c6a0463413e721fc94c1587fcc875
Greg Scott <me@gregoryjscott.com>
2023-02-01 09:58:30 -0600

    Install lerna with nx

---
+++ packages/eslint-config-custom/index.js
@@ -0,0 +1,33 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "import", "prettier"],
+  extends: [
+    "prettier",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+  overrides: [],
+  rules: {
+    "prettier/prettier": "error",
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        args: "none",
+      },
+    ],
+    "prefer-const": [
+      "error",
+      {
+        ignoreReadBeforeAssign: true,
+        destructuring: "all",
+      },
+    ],
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "import/no-extraneous-dependencies": "error",
+    "@typescript-eslint/no-empty-interface": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "react/jsx-key": "off",
+  },
+}
