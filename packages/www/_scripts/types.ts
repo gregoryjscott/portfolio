e9cf415b968971b23dfbfd521051390eaabf41b4
Greg Scott <me@gregoryjscott.com>
2025-07-02 10:49:12 -0500

    Reorganize types

--- packages/www/_scripts/types.ts
+++ packages/www/_scripts/types.ts
@@ -1,34 +1,5 @@
 import { resourceDirectories } from "./get-resources"
 
-export type Relation = typeof resourceDirectories[number] | "self"
-
-export type Link = { href: string }
-
-export type Links = {
-  [key in Relation]?: Link | Link[]
-}
-
-export type Markdown = {
-  frontmatter: Frontmatter
-  content: string
-}
-
-export type Frontmatter = {
-  [key: string]: any
-  _links?: Links
-}
-
-export type Embedded = {
-  [key in Relation]?: Yaml[]
-}
-
-export type Yaml = {
-  [key: string]: any
-  _links?: Links
-  _embedded?: Embedded
-  content?: string
-}
-
 export type Resource = {
   relation: Relation
   href: string
@@ -48,6 +19,35 @@ export type Resource = {
     }
     yaml: Yaml | undefined
   }
+}
+
+export type Relation = typeof resourceDirectories[number] | "self"
+
+export type Link = { href: string }
+
+export type Links = {
+  [key in Relation]?: Link | Link[]
+}
+
+export type Embedded = {
+  [key in Relation]?: Yaml[]
+}
+
+export type Markdown = {
+  frontmatter: Frontmatter
+  content: string
+}
+
+export type Frontmatter = {
+  [key: string]: any
+  _links?: Links
+}
+
+export type Yaml = {
+  [key: string]: any
+  _links?: Links
+  _embedded?: Embedded
+  content?: string
 }
 
 export type Sortable = Yaml & {
