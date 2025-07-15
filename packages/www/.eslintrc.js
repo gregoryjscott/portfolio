0daf71bcab132ef63e2fd48c7c7466bf7c8a87ff
Greg Scott <me@gregoryjscott.com>
2025-07-14 19:29:10 -0500

    Eliminate eslint-config-custom package

--- packages/www/.eslintrc.js
+++ packages/www/.eslintrc.js
@@ -1,4 +1,34 @@
 module.exports = {
   root: true,
-  extends: ["custom"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "import", "prettier"],
+  extends: [
+    "prettier",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:markdown/recommended",
+    "plugin:prettier/recommended",
+  ],
+  overrides: [],
+  rules: {
+    "prettier/prettier": "error",
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        args: "none",
+      },
+    ],
+    "prefer-const": [
+      "error",
+      {
+        ignoreReadBeforeAssign: true,
+        destructuring: "all",
+      },
+    ],
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "import/no-extraneous-dependencies": "error",
+    "@typescript-eslint/no-empty-interface": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "react/jsx-key": "off",
+  },
 }
